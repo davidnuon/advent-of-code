96ab3ff7525a7e719f5041f6961e7baa6fe766ba
David Nuon <davidnuongm@gmail.com>
2022-12-09 19:30:04 -0800

    Overwrite day 2 solution with the newer python implementation

--- 2022/02/main.py
+++ 2022/02/main.py
@@ -1,74 +1,63 @@
 #!/usr/bin/env python3
 
 import sys
+from enum import Enum
+
+
+class Shape(Enum):
+    Rock = 1
+    Paper = 2
+    Scissors = 3
+
+
+class Result(Enum):
+    Win = 6
+    Draw = 3
+    Lose = 0
+
+
+# This shape -> beats this shape
+WINNING_RULES = {
+    Shape.Rock: Shape.Scissors,
+    Shape.Paper: Shape.Rock,
+    Shape.Scissors: Shape.Paper
+}
+
+# Inverse map of winning rules
+LOSING_RULES = {v: k for (k, v) in WINNING_RULES.items()}
+
+
+def calc_matchup(them, us):
+    if them == us:
+        return Result.Draw
+    elif (them, us) in WINNING_RULES.items():
+        return Result.Lose
+
+    return Result.Win
 
 
 def rps_encode(opponent, player):
     opp_map = {
-        'A': 'Rock',
-        'B': 'Paper',
-        'C': 'Scissors'
+        'A': Shape.Rock,
+        'B': Shape.Paper,
+        'C': Shape.Scissors,
+        'X': Shape.Rock,
+        'Y': Shape.Paper,
+        'Z': Shape.Scissors
     }
 
-    player_map = {
-        'X': 'Rock',
-        'Y': 'Paper',
-        'Z': 'Scissors'
+    return [opp_map[opponent], opp_map[player]]
+
+
+def needed_result_encode(code):
+    result_map = {
+        'X': Result.Lose,
+        'Y': Result.Draw,
+        'Z': Result.Win
     }
 
-    return [opp_map[opponent], player_map[player]]
+    return result_map[code]
 
-
-def calc_rps_result(opponent, player):
-    match [opponent, player]:
-        case [opponent, 'Rock']:
-            return 1 + ({
-                'Rock': 3,
-                'Paper': 0,
-                'Scissors': 6
-            })[opponent]
-        case [opponent, 'Paper']:
-            return 2 + ({
-                'Rock': 6,
-                'Paper': 3,
-                'Scissors': 0
-            })[opponent]
-        case [opponent, 'Scissors']:
-            return 3 + ({
-                'Rock': 0,
-                'Paper': 6,
-                'Scissors': 3
-            })[opponent]
-
-
-def calc_matchup(left, right):
-    opponent_play = rps_encode(left, right)[0]
-    player_play = None
-    match [opponent_play, right]:
-        # Lose
-        case [opponent, 'X']:
-            player_play = ({
-                'Rock': 'Scissors',
-                'Paper': 'Rock',
-                'Scissors': 'Paper'
-            })[opponent]
-        # Draw
-        case [opponent, 'Y']:
-            player_play = ({
-                'Rock': 'Rock',
-                'Paper': 'Paper',
-                'Scissors': 'Scissors'
-            })[opponent]
-
-        # Win
-        case [opponent, 'Z']:
-           player_play = ({
-                'Scissors': 'Rock',
-                'Rock': 'Paper',
-                'Paper': 'Scissors'
-            })[opponent]
-
-    return [opponent_play, player_play]
 
 if __name__ == '__main__':
     problem_input = sys.stdin.read().strip().split('\n')
@@ -77,9 +66,25 @@ if __name__ == '__main__':
     part_1_points = 0
     part_2_points = 0
 
-    for [left, right] in plays:
-        part_1_points += calc_rps_result(*rps_encode(left, right))
-        part_2_points += calc_rps_result(*calc_matchup(left, right))
+    for play in plays:
+        [them, us] = rps_encode(*play)
+        result = calc_matchup(them, us)
+
+        part_1_points += result.value + us.value
+
+        needed_result = needed_result_encode(play[1])
+
+        match needed_result:
+            case Result.Win:
+                needed_play = LOSING_RULES[them]
+
+            case Result.Lose:
+                needed_play = WINNING_RULES[them]
+
+            case Result.Draw:
+                needed_play = them
+
+        part_2_points += needed_result.value + needed_play.value
 
     print('Part 1:', part_1_points)
     print('Part 2:', part_2_points)
